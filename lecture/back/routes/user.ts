68c215b7cd68b97a61d0263fc4fb6eb6de2a11ad
emsbn <emsbn@ctsoft.kr>
2020-03-31 18:57:19 +0900

    fix: routes, types

--- lecture/back/routes/user.ts
+++ lecture/back/routes/user.ts
@@ -39,8 +39,11 @@
 
 import * as express from 'express';
 import * as bcrypt from 'bcrypt';
-import { isLoggedIn } from './middleware';
+import * as passport from 'passport';
+
+import { isLoggedIn, isNotLoggedIn } from './middleware';
 import User from '../models/user';
+import Post from '../models/post';
 
 const router = express.Router();
 
@@ -71,4 +74,50 @@ router.post('/', async (req, res, next) => {
     console.log(error);
     next(error);
   }
+});
+
+router.post('/login', isNotLoggedIn, (req, res, next) => {
+  passport.authenticate('local', (err: Error, user: User, info: { message: string }) => {
+    if (err) {
+      console.log(err);
+      return next(err);
+    }
+    if (info) {
+      return res.status(401).send(info.message);
+    }
+    return req.login(user, async (loginErr: Error) => {
+      try {
+        if (loginErr) {
+          return next(loginErr);
+        }
+        const fullUser = await User.findOne({
+          where: { id: user.id },
+          include: [
+            {
+              model: Post,
+              as: 'Posts',
+              attributes: ['id'],
+            },
+            {
+              model: User,
+              as: 'Followings',
+              attributes: ['id'],
+            },
+            {
+              model: User,
+              as: 'Followers',
+              attributes: ['id'],
+            },
+          ],
+          attributes: {
+            exclude: ['password'],
+          },
+        });
+        return res.json(fullUser);
+      } catch (e) {
+        console.error(e);
+        return next(e);
+      }
+    });
+  });
 });
